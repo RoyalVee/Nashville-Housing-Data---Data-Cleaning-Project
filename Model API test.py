dab3c21d4225744aa6ee8ba251cee2320b0878c1
RoyalVee <victoroguche@gmail.com>
2022-02-27 19:46:55 +0100

    Feat : add price ML model to get function
    Added the ml model to return the price based on house details input

--- Model API test.py
+++ Model API test.py
@@ -4,5 +4,7 @@ import requests
 
 Base = "http://127.0.0.1:5000/"
 
-response  = requests.get(Base + "sale_prediction")
+response  = requests.get(Base + "sale_prediction", {"Property Area" : 10, "Sale Date" : 11, "Acreage" : 12, "LandValue": 13 ,
+       "BuildingValue" : 14, "TotalValue" : 15, "YearBuilt" : 16, "Bedrooms" : 17, "FullBath" : 18,
+       "HalfBath" : 19})
 print(response.json())
--- Price Model API.py
+++ Price Model API.py
@@ -14,18 +14,72 @@ import pandas as pd
 import numpy as np
 
 
+#get price function
+def get_price(Area, saledate_float, Acreage, LandValue,
+       BuildingValue, TotalValue, YearBuilt, Bedrooms, FullBath,
+       HalfBath):
+    """"
+    This function loads the machine learning model and predict the sale price
+    :arg Area, sale date, Acreage, LandValue,
+       BuildingValue, TotalValue, YearBuilt, Bedrooms, FullBath,
+       HalfBath
+    :return sale price predicted"""
+    input_dict = {"Area" : Area, "saledate_float" : saledate_float, "Acreage" : Acreage, "LandValue": LandValue ,
+       "BuildingValue" : BuildingValue, "TotalValue" : TotalValue, "YearBuilt" : YearBuilt, "Bedrooms" : Bedrooms, "FullBath" : FullBath,
+       "HalfBath" : HalfBath}
+    input_data = pd.DataFrame(data = input_dict, index = [0], columns = ['Area', 'saledate_float', 'Acreage', 'LandValue',
+       'BuildingValue', 'TotalValue', 'YearBuilt', 'Bedrooms', 'FullBath',
+       'HalfBath'])
+
+    # load Machine learning model
+    with open("SalePriceModel.pkl", "rb") as file:
+        reg = pk.load(file)
+
+    # predict sale price for input provided
+    SalePrice = reg.predict(input_data.to_numpy())
+
+    return SalePrice
 
 #create a flask instance for the API
 sale_app = Flask(__name__)
 sale_api = Api(sale_app)
+
+#create a request parser for collecting the required parameter for sale price prediction.
+house_details_arg = reqparse.RequestParser()
+house_details_arg.add_argument('Property Area', type = int, help = "Property Area is required", required = True)
+house_details_arg.add_argument('Sale Date', type = int, help = "Sale Date is required", required = True)
+house_details_arg.add_argument('Acreage', type = int, help = "Acreage is required", required = True)
+house_details_arg.add_argument('LandValue', type = int, help = "LandValue is required", required = True)
+house_details_arg.add_argument('BuildingValue', type = int, help = "BuildingValue is required", required = True)
+house_details_arg.add_argument('TotalValue', type = int, help = "TotalValue is required", required = True)
+house_details_arg.add_argument('YearBuilt', type = int, help = "YearBuilt is required", required = True)
+house_details_arg.add_argument('Bedrooms', type = int, help = "Bedrooms is required", required = True)
+house_details_arg.add_argument('FullBath', type = int, help = "FullBath is required", required = True)
+house_details_arg.add_argument('HalfBath', type = int, help = "HalfBath is required", required = True)
 
 
 # Create the Price model resource
 class sale_price(Resource):
     """"Create the price model methods """
     def get(self):
-        sale_pred = 12345
-        return {"Sale Price" : sale_pred}
+        args = house_details_arg.parse_args()
+
+        Property_Area = args['Property Area']
+        Sale_Date = args['Property Area']
+        Acreage = args['Property Area']
+        LandValue = args['Property Area']
+        BuildingValue = args['Property Area']
+        TotalValue = args['Property Area']
+        YearBuilt = args['Property Area']
+        Bedrooms = args['Property Area']
+        FullBath = args['Property Area']
+        HalfBath = args['Property Area']
+
+        pred_sale_price = get_price(Area= Property_Area, saledate_float= Sale_Date, Acreage= Acreage, LandValue=LandValue,
+                               BuildingValue=BuildingValue, TotalValue=TotalValue, YearBuilt=YearBuilt, Bedrooms=Bedrooms,
+                               FullBath= FullBath, HalfBath=HalfBath)
+
+        return {"Sale Price" : pred_sale_price}
 
 
 # Register the resource in the sale api
